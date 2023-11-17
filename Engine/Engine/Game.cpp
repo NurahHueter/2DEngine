f34b74dc12b429658896ee49c9bdbb44a861fe5b
Florian Rauter <frauter.mmt-b2022@fh-salzburg.ac.at>
2023-11-17 13:51:40 +0100

    feat: WindowManager geupdated

--- Engine/Engine/Game.cpp
+++ Engine/Engine/Game.cpp
@@ -11,8 +11,8 @@
 	void Game::Initialize()
 	{
 		//InputManager::instance().Init(m_window);  //pointer?
-		InputManager::instance().bind("switch", sf::Keyboard::Key::Space,1);
-		InputManager::instance().bind("music", sf::Keyboard::Key::W,1);
+		InputManager::instance().bind("switch", sf::Keyboard::Key::Space);
+		InputManager::instance().bind("music", sf::Keyboard::Key::W);
 		GameStateManager::instance().Init();
 		GameStateManager::instance().setState("MainState");	
 	};
@@ -35,7 +35,7 @@
 	void Game::Update(float deltaTime)
 	{
 
-		if (InputManager::instance().isKeyDown("music",1))
+		if (InputManager::instance().isKeyDown("music"))
 		{
 			if (AssetManager::instance().m_Music.find("cooleMusik") != AssetManager::instance().m_Music.end())
 			{
@@ -48,7 +48,7 @@
 		}
 		
 
-		if (InputManager::instance().isKeyUp("switch",1))
+		if (InputManager::instance().isKeyUp("switch"))
 		{
 			if (m_isGameInMenu)
 			{
@@ -75,7 +75,6 @@
 			if (event.type == sf::Event::Closed)
 			{
 				m_window.close();
-				closed = true;
 			}
 			CloseGame(event.key);
 			InputManager::instance().handleEvents(event);
--- Engine/Engine/Game.h
+++ Engine/Engine/Game.h
@@ -11,10 +11,7 @@ public:
 	const int m_HEIGHT = 600;
 	sf::VideoMode m_mode = { 800, 600 };
 	Game() : m_window(m_mode, "SFML Window")
-	{
-		m_window.setVerticalSyncEnabled(true);
-		//m_window.KeyPressed += CloseGame;		
-	}
+
 	sf::Color m_bg_color = { 0, 0, 255 };
 
 private: 
